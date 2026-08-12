3a1565145ef8c296af07069a1044a24cc2e23b08
mobieggez <wahlluvheart@gmail.com>
2026-08-12 12:01:40 +0700

    vv

--- images/script.js
+++ images/script.js
@@ -1,9 +1,12 @@
-const stickerbtn = document.getElementById('stickerbtn');
-
-stickerbtn.addEventListener('click', function() {
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: {y:0.3}
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    const stickerbtn = document.getElementById('stickerbtn');
+    if (stickerbtn) {
+        stickerbtn.addEventListener('click', function() {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+        });
+    }
 });
