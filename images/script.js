c33f07cc57b1a5ab41dfce3a28ff2a06dadba4de
mobieggez <wahlluvheart@gmail.com>
2026-08-12 09:53:18 +0700

    test

--- images/script.js
+++ images/script.js
@@ -1,3 +1,9 @@
-const stickerBtn = document.getElementById('stickerBtn');
+const stickerbtn = document.getElementById('stickerbtn');
 
-stickerBtn.addEventListener('click', function()
+stickerBtn.addEventListener('click', function() {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: {y:0.3}
+    });
+});
