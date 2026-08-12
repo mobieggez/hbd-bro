d1b058db64a17645d46fda64eff102e02c9cc477
mobieggez <wahlluvheart@gmail.com>
2026-08-12 11:39:17 +0700

    habzhuh

--- images/script.js
+++ images/script.js
@@ -1,6 +1,6 @@
 const stickerbtn = document.getElementById('stickerbtn');
 
-stickerBtn.addEventListener('click', function() {
+stickerbtn.addEventListener('click', function() {
     confetti({
         particleCount: 100,
         spread: 70,
